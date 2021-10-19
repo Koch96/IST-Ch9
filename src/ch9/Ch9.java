61a4287a17acd372cf8cfd07ac0b81d9352a3ac5
Stephen Koch <stephen.koch96@gmail.com>
2021-10-19 12:58:35 -0400

    redone initial commit

--- src/ch9/Ch9.java
+++ src/ch9/Ch9.java
@@ -1,13 +1,17 @@
 
 package ch9;
 
+//import java.awt.Rectangle;
+//import java.util.Scanner;
+import javax.swing.JOptionPane;
+
 
 //@author sak5680
 
 public class Ch9 {
 
     public static void main(String[] args) {
-        
+        String name = JOptionPane.showInputDialog("What is your name?");
     }
 
 }
